327267c6e680bb9d14f19982d0e4206a47b14151
Michael08039 <mikegaston99@yahoo.com>
2023-06-27 00:13:47 -0700

    4 hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
+ *
  */
 int main(void)
 {
-    return (0);
-}
-        put("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
