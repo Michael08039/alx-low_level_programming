a1216a4db38c973cf1c6caa2b6b6be3c454e1158
Michael08039 <mikegaston99@yahoo.com>
2023-07-06 04:34:44 -0700

    recursion 01

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,18 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * _print_rev_recursion - print a string in reverse
- *
- * @s: char to check
- *
- * Return: 0 is success
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string
+ * Return: pointer
  */
+
 void _print_rev_recursion(char *s)
+
 {
-	if (*s)
+	if (*s != '\0')
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		_print_rev_recursion((s + 1));
+		_putchar(*(s + 0));
 	}
+
 }
